bc4c8f8f1a66845fb75255cfdadf50acb133c912
mrgruchith <43485830+mrgruchith@users.noreply.github.com>
2019-11-25 12:38:24 +0530

    Update server.php

--- server.php
+++ server.php
@@ -8,6 +8,8 @@ $errors = array();
 
 
 $db = mysqli_connect('us-cdbr-iron-east-05.cleardb.net', 'ba8f873b48788d', '656e0de9', 'heroku_cf0099486ad9e78');
+if($db==NULL)
+	echo("error");
 
 if (isset($_POST['reg_user'])) {
  
