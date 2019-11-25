a575485deec92b8625788b20bd8fe9f97273a2e8
mrgruchith <43485830+mrgruchith@users.noreply.github.com>
2019-11-25 12:21:09 +0530

    Update server.php

--- server.php
+++ server.php
@@ -42,7 +42,7 @@ if (isset($_POST['reg_user'])) {
   if (count($errors) == 0) {
   	$password = md5($password_1);
 
-  	$query = "INSERT INTO users (username, email, password) 
+  	$query = "INSERT INTO registration (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
