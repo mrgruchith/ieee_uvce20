6e1e259f5c0b9e0717869d8cbec9a575c0283e64
mrgruchith <43485830+mrgruchith@users.noreply.github.com>
2019-11-27 11:36:41 +0530

    Update server.php

--- server.php
+++ server.php
@@ -7,21 +7,9 @@ $email    = "";
 $errors = array(); 
 
 
-$db = mysqli_connect('us-cdbr-iron-east-05.cleardb.net', 'ba8f873b48788d', '656e0de9', 'heroku_cf0099486ad9e78');
-
+$db = mysqli_connect('localhost', 'root', '', 'registration');
 
 if (isset($_POST['reg_user'])) {
-      
-     $db = mysqli_connect('us-cdbr-iron-east-05.cleardb.net', 'ba8f873b48788d', '656e0de9', 'heroku_cf0099486ad9e78');
-if($db==NULL)
-{
-	?>
-  <script>
-  alert('error while registering file');
-        window.location.href='register.php?fail';
-        </script>
-<?php
-      }
  
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
@@ -36,7 +24,7 @@ if($db==NULL)
 	array_push($errors, "The two passwords do not match");
   }
 
-  $user_check_query = "SELECT * FROM registration WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -54,12 +42,12 @@ if($db==NULL)
   if (count($errors) == 0) {
   	$password = md5($password_1);
 
-  	$query = "INSERT INTO registration (username, email, password) 
+  	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: register.php');
+  	header('location: index.php');
   }
 }
 
@@ -79,7 +67,7 @@ if (isset($_POST['login_user'])) {
     if (count($errors) == 0) {
         //$password = md5($password);
         
-        $query = "SELECT * FROM registration WHERE username='$username' AND password='$password'";
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if ($username == 'ieeeuvce' && $password == 'ieeeuvce@2019')  {
           $_SESSION['username'] = $username;
