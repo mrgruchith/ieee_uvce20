586087427fe27b73c6f58ae0367a989375bdaecf
mrgruchith <43485830+mrgruchith@users.noreply.github.com>
2019-11-28 15:20:39 +0530

    Update server.php

--- server.php
+++ server.php
@@ -78,7 +78,7 @@ if (isset($_POST['login_user'])) {
         elseif (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
           $_SESSION['success'] = "You are now logged in";
-          header('location: index.php');
+          header('location: loginindex.php');
         }
         else {
             array_push($errors, "Wrong username/password combination");
