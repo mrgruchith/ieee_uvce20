5e99d27774b67c170ee8f277b9f364d881412fc1
mrgruchith <43485830+mrgruchith@users.noreply.github.com>
2019-11-28 15:18:03 +0530

    Update server.php

--- server.php
+++ server.php
@@ -48,7 +48,7 @@ if (isset($_POST['reg_user'])) {
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+  	header('location: loginindex.php');
   }
 }
 
